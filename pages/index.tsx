4a22dde2c73b33c69c1d8ddd9e6b17f6cab3edfb
Alvaro_Aquije <gonzalo_6824@outlook.com>
2021-08-25 15:21:44 -0500

    feat(root): :sparkles: initial build and testing for Vercel

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,20 @@
-import Head from 'next/head'
+import Head from "next/head";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>FunBio Equipo 6</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
         <h1 className="text-6xl font-bold">
-          Welcome to{' '}
+          Proyecto Biodiseño{" "}
           <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
+            Equipo 6
           </a>
         </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
 
         <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
           <a
@@ -73,10 +66,10 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
         </a>
       </footer>
     </div>
-  )
+  );
 }
