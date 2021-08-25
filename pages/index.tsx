bfd65d1ffb5111ff86cecb68198740c29d1b64cd
Alvaro_Aquije <gonzalo_6824@outlook.com>
2021-08-25 16:31:47 -0500

    feat(components): :lipstick: minor updates - styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,14 +6,17 @@ export default function Home() {
       <Head>
         <title>FunBio Equipo 6</title>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
+          rel="stylesheet"
+        />
       </Head>
 
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Proyecto Biodiseño{" "}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Equipo 6
-          </a>
+        <h1 className="text-6xl font-extrabold">
+          Proyecto Biodiseño <p className="text-blue-600">Equipo 6</p>
         </h1>
 
         <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
@@ -21,8 +24,8 @@ export default function Home() {
             href="https://nextjs.org/docs"
             className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
           >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
+            <h3 className="text-xl font-bold">Documentation &rarr;</h3>
+            <p className="mt-4 ">
               Find in-depth information about Next.js features and API.
             </p>
           </a>
@@ -31,8 +34,8 @@ export default function Home() {
             href="https://nextjs.org/learn"
             className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
           >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
+            <h3 className="text-xl font-bold">Learn &rarr;</h3>
+            <p className="mt-4 text-md">
               Learn about Next.js in an interactive course with quizzes!
             </p>
           </a>
@@ -41,8 +44,8 @@ export default function Home() {
             href="https://github.com/vercel/next.js/tree/master/examples"
             className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
           >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
+            <h3 className="text-xl font-bold">Examples &rarr;</h3>
+            <p className="mt-4 text-md">
               Discover and deploy boilerplate example Next.js projects.
             </p>
           </a>
@@ -51,22 +54,32 @@ export default function Home() {
             href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
           >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
+            <h3 className="text-xl font-bold">Deploy &rarr;</h3>
+            <p className="mt-4 text-md">
               Instantly deploy your Next.js site to a public URL with Vercel.
             </p>
           </a>
         </div>
       </main>
 
-      <footer className="flex items-center justify-center w-full h-24 border-t">
+      <footer className="flex items-center justify-center w-full h-24 border-t flex-col space-y-2">
+        <p>
+          Developed By{" "}
+          <a
+            href="https://github.com/AlvaroAquijeDiaz"
+            className={`text-blue-700 font-bold underline`}
+          >
+            Alvaro Aquije
+          </a>{" "}
+          &amp; Team
+        </p>
         <a
           className="flex items-center justify-center"
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{" "}
+          {" "}
           <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
         </a>
       </footer>
